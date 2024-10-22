4340ed5a5786ea1c95c19e700cce2b7f07f887bc
saintworld09 <ababatunde306@gnail.com>
2024-10-22 11:49:03 -0700

    Week 6 assignment

--- prepare/Learning06/Program.cs
+++ prepare/Learning06/Program.cs
@@ -1,9 +1,21 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning06 World!");
+        List<Shape> shapes = new List<Shape>
+        {
+            new Square("Red", 4),
+            new Rectangle("Blue", 5, 3),
+            new Circle("Green", 2.5)
+        };
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Shape Color: {shape.GetColor()}");
+            Console.WriteLine($"Shape Area: {shape.GetArea()}\n");
+        }
     }
 }
